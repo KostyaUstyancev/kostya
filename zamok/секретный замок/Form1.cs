48092060cc92f7bef300854b7853052c680f9f12
KostyaUstyancev <43114158+KostyaUstyancev@users.noreply.github.com>
2018-10-18 09:11:46 +0500

    tests
    тесты

--- zamok/секретный замок/Form1.cs
+++ zamok/секретный замок/Form1.cs
@@ -24,35 +24,43 @@ namespace секретный_замок
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
                 int[] mas = new int[10] {Convert.ToInt32(Box1.Text), Convert.ToInt32(Box2.Text), Convert.ToInt32(Box3.Text), Convert.ToInt32(Box4.Text),
                                      Convert.ToInt32(Box5.Text), Convert.ToInt32(Box6.Text), Convert.ToInt32(Box7.Text), Convert.ToInt32(Box8.Text),
                                      Convert.ToInt32(Box9.Text), Convert.ToInt32(Box10.Text)};
-            
-            
-            for (int i = 0;i<=mas.Length-1;i++)
+
+            Proverka g = new Proverka();
+            string res = g.sravnenie(mas);
+            if (res == "Замок открыт")
             {
-                if (i + 1 < mas.Length-1 || i + 2 < mas.Length-1)
+                text1.BackColor = Color.GreenYellow;
+            }
+            else
+            {
+                text1.BackColor = Color.Red;
+            }
+                
+        }
+    }
+    public class Proverka
+    {
+        public string sravnenie(int[] s)
+        {
+            for (int i = 0; i <= s.Length - 1; i++)
+            {
+                if (i + 1 < s.Length - 1 || i + 2 < s.Length - 1)
                 {
-                   
-                        if (mas[i + 1] + mas[i] + mas[i + 2] == 10)
-                        {
-                            text1.Text = "Замок открыт";
-                            text1.BackColor = Color.GreenYellow;
-                            break;
-                        }
-                  
-                    
-                    
+                    if (s[i + 1] + s[i] + s[i + 2] == 10)
+                    {
+                        return "Замок открыт";
+                       
+                    }
                 }
                 else
                 {
-                    text1.Text = "Замок закрыт";
-                    text1.BackColor = Color.Red;
-                    break;
+                    return "Замок закрыт";                   
                 }
-              
             }
+            return "Ошибка";
         }
     }
 }
